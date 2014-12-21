03336b2d21c7b152930cb70d1bae3643f303c713
davidy <david.yuan@propellerhead.co.nz>
2014-12-22 09:01:00 +1300

    test

--- bookshop-process-test/src/test/java/nz/co/bookshop/process/test/activitids/DeploymentDSIntegrationTest.groovy
+++ bookshop-process-test/src/test/java/nz/co/bookshop/process/test/activitids/DeploymentDSIntegrationTest.groovy
@@ -2,6 +2,8 @@
 
 package nz.co.bookshop.process.test.activitids;
 
+import static org.hamcrest.CoreMatchers.is
+import static org.hamcrest.CoreMatchers.not
 import static org.junit.Assert.*
 import groovy.util.logging.Slf4j
 import nz.co.bookshop.process.ActivitiSupportModule
@@ -11,10 +13,11 @@ import nz.co.bookshop.process.ds.DeploymentDS
 import nz.co.bookshop.process.test.GuiceJUnitRunner
 import nz.co.bookshop.process.test.GuiceJUnitRunner.GuiceModules
 
-import org.junit.After
-import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 
 import com.google.common.io.Resources
 import com.google.inject.Inject
@@ -22,37 +25,64 @@ import com.google.inject.Inject
 @RunWith(GuiceJUnitRunner.class)
 @GuiceModules([ConfigurationServiceModule.class, SharedModule.class, ActivitiSupportModule.class ])
 @Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class DeploymentDSIntegrationTest {
 
 	static final String TEST_BPMN_BAR="laptopOrderHumanProcess.bar"
 	static final String TEST_PROCESS_NAME="laptop"
 	static final String TEST_PROCESS_CATEGORY="order"
 
-	File uploadBarFile
-	String testDeploymentId
+	static File uploadBarFile
+	static String testDeploymentId
 
 	@Inject
 	DeploymentDS deploymentDs
 
-	@Before
-	void setUp(){
+	@BeforeClass
+	static void setUp(){
 		uploadBarFile = new File(Resources.getResource(TEST_BPMN_BAR).file)
-		Map<String,String> metaMap = deploymentDs.deployment("laptop", "order", uploadBarFile)
-		log.info "metaMap:{} $metaMap"
-		testDeploymentId = metaMap['id']
 	}
 
-	@After
-	void tearDown(){
-		if(testDeploymentId){
-			deploymentDs.unDeployment(testDeploymentId)
-		}
+	//	@After
+	//	void tearDown(){
+	//		if(testDeploymentId){
+	//			deploymentDs.unDeployment(testDeploymentId)
+	//		}
+	//	}
+
+	@Test
+	void deployment(){
+		Map<String,String> metaMap = deploymentDs.deployment("laptop", "order", uploadBarFile)
+		assertThat(metaMap['tenantId'], is("laptop:order"))
+		assertNotNull(metaMap['id'])
+		testDeploymentId = metaMap['id']
+	}
+	
+	@Test
+	void getDeploymentById(){
+		Map<String,String> resultMap = deploymentDs.getDeployment(testDeploymentId)
+		assertNotNull(resultMap)
+		assertThat(resultMap['tenantId'], is("laptop:order"))
 	}
 
 	@Test
-	void testGetDeploymentByNameAndCategory(){
-		Map<String,String> resultMap = deploymentDs.getDeployment(TEST_PROCESS_NAME, TEST_PROCESS_CATEGORY)
-		assertNotNull(resultMap)
+	void getDeploymentByNameAndCategory(){
+		Map<String,Object> resultMap = deploymentDs.getDeployment(TEST_PROCESS_NAME, TEST_PROCESS_CATEGORY)
 		log.info "resultMap:{} $resultMap"
+		assertNotNull(resultMap)
+		Map<String,String> dataMap = ((List)resultMap['data']).first()
+		assertThat(dataMap['tenantId'], is("laptop:order"))
+	}
+	
+	@Test
+	void getDeploymentResources(){
+		List<Map<String,Object>> deployResources = deploymentDs.getDeploymentResource(testDeploymentId)
+		println "deployResources:{} $deployResources"
+	}
+
+	@Test
+	void undeployment(){
+		println "testDeploymentId:{} $testDeploymentId"
+		deploymentDs.unDeployment(testDeploymentId)
 	}
 }
